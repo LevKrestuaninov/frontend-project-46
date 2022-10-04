c4d3a2ccb16120cb299511419bae020646f2f34f
forever-yes <molotov.eugene@gmail.com>
2022-10-04 23:35:33 +0500

    add formatter's default value

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,12 +8,12 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version('1.0.2')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    console.log(getDifference(filepath1, filepath2));
+  .option('-f, --format <formatter>', 'output format', 'stylish')
+  .action((filepath1, filepath2, formatter) => {
+    console.log(getDifference(filepath1, filepath2, formatter));
   })
   .parse(process.argv);
 
