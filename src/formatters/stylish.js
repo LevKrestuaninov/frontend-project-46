565544a8de5cb08bd80109a8e9ec4f8700895410
forever-yes <molotov.eugene@gmail.com>
2022-10-05 19:24:33 +0500

    minor style fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,7 +1,11 @@
 import _ from 'lodash';
 
 const stylish = (diff) => {
-  const diffSymbol = { added: '  + ', deleted: '  - ', equal: '    ' };
+  const diffSymbol = {
+    added: '  + ',
+    deleted: '  - ',
+    equal: '    ',
+  };
 
   const iter = (nest, depth) => {
     if (!_.isPlainObject(nest)) {
@@ -10,20 +14,22 @@ const stylish = (diff) => {
 
     const endBracketPadding = '    '.repeat(depth);
     const padding = `${'    '.repeat(depth + 1)}`;
-    const sortedObj = Object.entries(nest).sort((a, b) => a[0] < b[0]);
+    const sorted = Object.entries(nest).sort((a, b) => a[0] < b[0]);
 
-    const lines = sortedObj.reduce((acc, [key, value]) => {
+    const lines = sorted.reduce((acc, [key, value]) => {
       const lineMaker = (k = key, v = value, p = padding) => `${p}${k}: ${iter(v, depth + 1)}`;
       const status = (value || { }).diff_status;
-      if (status && status !== 'changed') {
-        const newPadding = `${'    '.repeat(depth)}${diffSymbol[status]}`;
-        return [...acc, lineMaker(key, nest[key].value, newPadding)];
-      }
+
       if (status === 'changed') {
         const newPadding = `${'    '.repeat(depth)}${diffSymbol.deleted}`;
         const newPadding2 = `${'    '.repeat(depth)}${diffSymbol.added}`;
         return [...acc, lineMaker(key, nest[key].value, newPadding),
           lineMaker(key, nest[key].value2, newPadding2)];
+      }
+
+      if (status) {
+        const newPadding = `${'    '.repeat(depth)}${diffSymbol[status]}`;
+        return [...acc, lineMaker(key, nest[key].value, newPadding)];
       }
 
       return [...acc, lineMaker()];
