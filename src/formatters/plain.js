4c49a5854e57a5d1e1c3ff0304f1c2715ee89053
forever-yes <molotov.eugene@gmail.com>
2022-10-05 19:17:57 +0500

    minor style fix

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,27 +1,34 @@
 import _ from 'lodash';
 
+const checkValue = (value) => {
+  let returnValue = null;
+  if (_.isPlainObject(value) || _.isString(value)) {
+    returnValue = _.isString(value) ? `'${value}'` : '[complex value]';
+  }
+
+  return returnValue;
+};
+
 const plain = (diff, path = []) => {
   const sorted = Object.entries(diff).sort((a, b) => a[0] < b[0]);
   const lines = sorted.reduce((acc, [key, value]) => {
     const currentPath = [...path, key];
     const status = (value || { }).diff_status;
-    if (status && status !== 'equal') {
-      let returnValue = value.value;
-      let returnValue2 = value.value2;
-      if (_.isPlainObject(returnValue) || _.isString(returnValue)) {
-        returnValue = _.isString(returnValue) ? `'${returnValue}'` : '[complex value]';
-      }
-      if (_.isPlainObject(returnValue2) || _.isString(returnValue2)) {
-        returnValue2 = _.isString(returnValue2) ? `'${returnValue2}'` : '[complex value]';
-      }
+
+    if (status === 'equal') {
+      return [...acc];
+    }
+
+    if (status) {
+      const returnValue = (checkValue(value.value) ?? value.value);
+      const returnValue2 = (checkValue(value.value2) ?? value.value2);
       const possibleStatus = {
         added: `was added with value: ${returnValue}`,
         deleted: 'was removed',
         changed: `was updated. From ${returnValue} to ${returnValue2}`,
       };
+
       return [...acc, `Property '${currentPath.join('.')}' ${possibleStatus[status]}`];
-    } if (status === 'equal') {
-      return [...acc];
     }
 
     return [...acc, plain(value, currentPath)];
