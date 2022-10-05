52e0afc5eeb55e35c1d7cf7d6eb010e423d19ec5
forever-yes <molotov.eugene@gmail.com>
2022-10-05 17:48:05 +0500

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import getPath from './makePath.js';
 import parsers from './parsers.js';
 import getDiff from './getDiff.js';
-import stylish from './stylish.js';
+import getFormatter from './formatters/index.js';
 
 const getDifference = (path1, path2, formatter) => {
   const [pathTo1, pathTo2] = [getPath(path1), getPath(path2)];
   const [file1, file2] = [parsers(pathTo1), parsers(pathTo2)];
   const diff = getDiff(file1, file2);
-  let toFormat = stylish;
+  const toFormat = getFormatter(formatter);
 
-  switch (formatter) {
-    case ('plain'):
-      toFormat = 'plain';
-      break;
-    default:
-      console.log('Unknown formatter');
-  }
   return toFormat(diff);
 };
 
